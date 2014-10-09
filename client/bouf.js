66baf67158aa922a4374b991cb07d6fb0189a951
nwabdou85 <nwabdou85@gmail.com>
2014-10-09 14:30:56 +0200

    First commit

---
+++ client/bouf.js
@@ -0,0 +1,28 @@
+
+// window.addEventListener("resize", function(){
+
+// 	var imgi = document.getElementById('img');
+// 	imgi.style.height = "100%";
+    // 	imgi.style.width = "100%";
+// });
+
+// Template.hello.helpers({
+// (function($) {window.fnames = new Array();
+//  window.ftypes = new Array();
+//  fnames[0]='EMAIL';ftypes[0]='email';
+//  fnames[1]='MMERGE1';ftypes[1]='zip';
+//  fnames[2]='MMERGE2';ftypes[2]='text';}(jQuery));
+// var $mcj = jQuery.noConflict(true);
+// });
+
+
+
+
+
+//  var email = $("form input[type='email']").val();
+
+// if (/^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/.test(email)) {
+//     alert("Adresse e-mail valide !");
+// } else {
+//     alert("Adresse e-mail invalide !");
+// }
